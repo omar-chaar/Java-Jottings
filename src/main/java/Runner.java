0a5bbf550e1d2cdd5bec44157449052283ca553a
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2023-03-24 03:07:59 +0200

    Added support for maven

---
+++ src/main/java/Runner.java
@@ -0,0 +1,24 @@
+import org.jbake.app.JBakeException;
+import org.jbake.app.Oven;
+
+import javax.naming.ConfigurationException;
+import java.io.File;
+
+public class Runner {
+    public static void main(String [] args){
+        try {
+            File source = new File("src/jbake-source/");
+            File destination = new File("src/output");
+            Oven oven = new Oven(source, destination, true);
+            oven.setupPaths();
+            oven.bake();
+        } catch (ConfigurationException e) {
+            System.out.println(e.getMessage());
+        } catch (JBakeException e) {
+            System.out.println(e.getMessage());
+            // do something with exception here
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
