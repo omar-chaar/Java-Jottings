b47dace659b5af01577cc089ac594cffc597f1fb
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2023-10-21 10:00:08 +0400

    Added dependency injection to Runner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import org.jbake.app.Oven;
+
+import java.io.File;
+
 public class Main {
-    public static void main(String[] args) {
-        Runner runner = new Runner();
+    public static void main(String[] args) throws Exception {
+        File source = new File("src");
+        File destination = new File("src/output");
+        Oven oven = new Oven(source, destination, true);
+        Runner runner = new Runner(oven);
         runner.run();
     }
 
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,25 +1,17 @@
 import org.jbake.app.JBakeException;
 import org.jbake.app.Oven;
 
-import javax.naming.ConfigurationException;
-import java.io.File;
-import java.io.InputStream;
-
 public class Runner {
-    public Runner(){
+    private final Oven oven;
+
+    public Runner(Oven oven) {
+        this.oven = oven;
     }
 
-    public void run(){
+    public void run() {
         try {
-            File source = new File("src");
-            File destination = new File("src/output");
-            Oven oven = new Oven(source, destination, true);
             oven.setupPaths();
             oven.bake();
-        } catch (ConfigurationException e) {
-            System.out.println(e.getMessage());
-        } catch (JBakeException e) {
-            System.out.println(e.getMessage());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
