c9d5077e9995e2b89cb065de78b5166eafcfa2eb
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2023-10-24 19:36:36 +0400

    Updated OvenFactory

--- src/main/java/OvenFactory.java
+++ src/main/java/OvenFactory.java
@@ -8,11 +8,11 @@ public class OvenFactory {
     public static final String DEFAULT_SOURCE_OUTPUT = "src/output";
     public static final boolean IS_CLEAR_CACHE = true;
 
-    public static Oven createOven() throws Exception {
+    public static Oven createOven(){
         return createOven(new File(DEFAULT_SOURCE), new File(DEFAULT_SOURCE_OUTPUT), IS_CLEAR_CACHE);
     }
 
-    public static Oven createOven(File source, File destination, boolean isClearCache) throws Exception {
+    public static Oven createOven(File source, File destination, boolean isClearCache){
         return new Oven(new JBakeConfigurationFactory().createDefaultJbakeConfiguration(source, destination, isClearCache));
     }
 }
